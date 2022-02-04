c0066f8d4cc8c7d55d3aed7b1fe7742a16ddd72f
Armen Avakian <aaavakn@gmail.com>
2022-02-04 18:05:55 +0300

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,27 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/template';
+import {resizeHandler} from '@/components/table/resize';
+import {shouldResize} from '@/components/table/functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      // Добавляем одно событие на корневой элемент, чтобы для каждой ячейки
+      // не делать этого. Это оптимизация, делегирование
+      listeners: ['mousedown', 'mouseup']
+    });
+  }
+
   toHTML() {
     return createTable(50);
+  }
+
+  onMousedown(event) {
+    // Получение атрибутов data-...
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
+    }
   }
 }
---
+++ src/components/table/resize.js
@@ -0,0 +1,39 @@
+import {$} from '@core/dom';
+
+export function resizeHandler($root, event) {
+  const $resizer = $(event.target);
+  // Не берем parent, используем атрибут, чтобы было меньше связи с View
+  const $parent = $resizer.closest('[data-type="resizable"]');
+  const coords = $parent.getCoords();
+  const type = $resizer.data.resize;
+  const lineProp = type === 'col' ? 'bottom' : 'right';
+  let value;
+
+  // Всегда показываем resizer и сделаем его длиннее
+  $resizer.css({opacity: 1, [lineProp]: '-5000px'});
+
+  document.onmousemove = e => {
+    if (type === 'col') {
+      const delta = e.pageX - coords.right;
+      value = coords.width + delta;
+      $resizer.css({right: `${-delta}px`});
+    } else {
+      const delta = e.pageY - coords.bottom;
+      value = coords.height + delta;
+      $resizer.css({bottom: `${-delta}px`});
+    }
+  };
+
+  document.onmouseup = () => {
+    if (type === 'col') {
+      $root
+          .findAll(`[data-col="${$parent.data.col}"]`)
+          .forEach(el => $(el).css({width: `${value}px`}));
+    } else {
+      $parent.css({height: `${value}px`});
+    }
+
+    document.onmousemove = null;
+    $resizer.css({opacity: 0, right: 0, bottom: 0});
+  };
+}
